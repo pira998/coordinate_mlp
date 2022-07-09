2b28d5692629ffbe6d848a4a7364d17777d5c8d9
Praveen <piraveen.18@cse.mrt.ac.lk>
2022-07-10 03:41:40 +0530

    change in hparams

--- opt.py
+++ opt.py
@@ -10,17 +10,17 @@ def get_opts():
     parser.add_argument('--arch', type=str, default='identity', choices=['identity'], help='Network architecture')
 
     # batch size with 4
-    parser.add_argument('--batch_size', type=int, default=4, help='Batch size')
+    parser.add_argument('--batch_size', type=int, default=256*256, help='Batch size')
 
     # epochs with 10
-    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs')
+    parser.add_argument('--epochs', type=int, default=2000, help='Number of epochs')
 
     # learning rate with 0.001
-    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--lr', type=float, default=0.0001, help='Learning rate')
 
     # experiment name
     parser.add_argument('--exp_name', type=str, default='exp', help='Experiment name')
-    
+
 
 
 
